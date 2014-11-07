a7d10f41cc147c20022f227bc08f24dd4fbc72e7
unknown <smirnovmb@SmirnovMB.svgc.ru>
2014-11-07 14:37:53 +0400

    Init

---
+++ SMBLabeledEdit/SMBLabeledEdit.pas
@@ -0,0 +1,344 @@
+unit SMBLabeledEdit;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Vcl.Controls, Vcl.StdCtrls, Vcl.ExtCtrls, Windows,
+  SMB.Validators, System.Variants;
+
+type
+  TValueType            = (vtString, vtInteger, vtFloat);
+  TValueConstraintType  = (vctNone, vctGreaterThan, vctGreaterThanOrEqual,
+    vctLessThan, vctLessThanOrEqual, vctBetween, vctBetweenOrEqual);
+
+type
+  TSMBLabeledEdit = class(TCustomLabeledEdit)
+  private
+    FIntValue: Integer;
+    FFloatValue: Extended;
+    FValid: Boolean;
+    FErrorMsg: String;
+    FFilterInput: Boolean;
+    FOnFilterFloatInput: TKeyPressEvent;
+    FOnFilterIntegerInput: TKeyPressEvent;
+    FOnFilterStringInput: TKeyPressEvent;
+    FValueType: TValueType;
+    FMin: Extended;
+    FMax: Extended;
+    FValueConstraint: TValueConstraintType;
+    FAutoValidate: Boolean;
+    procedure SetFilterInput(const Value: Boolean);
+    function GetValue: Variant;
+    procedure SetValue(const Value: Variant);
+    procedure SetValueConstraint(const Value: TValueConstraintType);
+    procedure ValidateString;
+    procedure ValidateInteger;
+    procedure ValidateFloat;
+    function GetValid: Boolean;
+    procedure SetAutoValidate(const Value: Boolean);
+    procedure SetValueType(const Value: TValueType);
+    procedure SetMax(const Value: Extended);
+    procedure SetMin(const Value: Extended);
+    function GetCaption: TCaption;
+    procedure SetCaption(const Value: TCaption);
+    function GetErrorMsg: String;
+  protected
+    procedure KeyPress(var Key: Char); override;
+    procedure FilterStringInput(var Key: Char); virtual;
+    procedure FilterIntegerInput(var Key: Char); virtual;
+    procedure FilterFloatInput(var Key: Char); virtual;
+    procedure DoValidate(); virtual;
+    procedure Change; override;
+  public
+    {TSMBLabeledEdit}
+    property ErrorMsg: String read GetErrorMsg;
+    property Valid: Boolean read GetValid;
+  published
+    property Alignment;
+    property Anchors;
+    property AutoSelect;
+    property AutoSize;
+    property BevelEdges;
+    property BevelInner;
+    property BevelKind;
+    property BevelOuter;
+    property BiDiMode;
+    property BorderStyle;
+    property CharCase;
+    property Color;
+    property Constraints;
+    property Ctl3D;
+    property DoubleBuffered;
+    property DragCursor;
+    property DragKind;
+    property DragMode;
+    property EditLabel;
+    property Enabled;
+    property Font;
+    property HideSelection;
+    property ImeMode;
+    property ImeName;
+    property LabelPosition;
+    property LabelSpacing;
+    property MaxLength;
+    property OEMConvert;
+    property ParentBiDiMode;
+    property ParentColor;
+    property ParentCtl3D;
+    property ParentDoubleBuffered;
+    property ParentFont;
+    property ParentShowHint;
+    property PasswordChar;
+    property PopupMenu;
+    property ReadOnly;
+    property ShowHint;
+    property TabOrder;
+    property TabStop;
+    property Text;
+    property TextHint;
+    property Touch;
+    property Visible;
+    property StyleElements;
+    property OnChange;
+    property OnClick;
+    property OnContextPopup;
+    property OnDblClick;
+    property OnDragDrop;
+    property OnDragOver;
+    property OnEndDock;
+    property OnEndDrag;
+    property OnEnter;
+    property OnExit;
+    property OnGesture;
+    property OnKeyDown;
+    property OnKeyPress;
+    property OnKeyUp;
+    property OnMouseActivate;
+    property OnMouseDown;
+    property OnMouseEnter;
+    property OnMouseLeave;
+    property OnMouseMove;
+    property OnMouseUp;
+    property OnStartDock;
+    property OnStartDrag;
+    {TSMBLabeledEdit}
+    property FilterInput: Boolean read FFilterInput write SetFilterInput default False;
+    property ValueType: TValueType read FValueType write SetValueType default vtString;
+    property Value: Variant read GetValue write SetValue;
+    property Min: Extended read FMin write SetMin;
+    property Max: Extended read FMax write SetMax;
+    property ValueConstraint: TValueConstraintType read FValueConstraint write SetValueConstraint default vctNone;
+    property AutoValidate: Boolean read FAutoValidate write SetAutoValidate default False;
+    property Caption: TCaption read GetCaption write SetCaption;
+    {Events}
+    property OnFilterFloatInput: TKeyPressEvent read FOnFilterFloatInput write FOnFilterFloatInput;
+    property OnFilterIntegerInput: TKeyPressEvent read FOnFilterIntegerInput write FOnFilterIntegerInput;
+    property OnFilterStringInput: TKeyPressEvent read FOnFilterStringInput write FOnFilterStringInput;
+  end;
+
+procedure Register;
+
+implementation
+
+procedure Register;
+begin
+  RegisterComponents('SMB Components', [TSMBLabeledEdit]);
+end;
+
+{ TSMBLabeledEdit }
+
+procedure TSMBLabeledEdit.Change;
+begin
+  inherited Change;
+  if FAutoValidate then DoValidate;
+end;
+
+//constructor TSMBLabeledEdit.Create(AOwner: TComponent);
+//begin
+//  inherited Create(AOwner);
+//  FIntValue   := 0;
+//  FFloatValue := 0;
+//  FValid      := True;
+//end;
+
+procedure TSMBLabeledEdit.DoValidate;
+begin
+  FErrorMsg := '';
+  case FValueType of
+    vtString:   ValidateString;
+    vtInteger:  ValidateInteger;
+    vtFloat:    ValidateFloat;
+  end;
+end;
+
+procedure TSMBLabeledEdit.FilterFloatInput(var Key: Char);
+begin
+  if FFilterInput then
+    if Assigned(FOnFilterFloatInput) then
+      FOnFilterFloatInput(Self, Key)
+    else
+      KeyPressFloat(Key);
+end;
+
+procedure TSMBLabeledEdit.FilterIntegerInput(var Key: Char);
+begin
+  if FFilterInput then
+    if Assigned(FOnFilterIntegerInput) then
+      FOnFilterIntegerInput(Self, Key)
+    else
+      KeyPressInt(Key);
+end;
+
+procedure TSMBLabeledEdit.FilterStringInput(var Key: Char);
+begin
+  if FFilterInput and Assigned(FOnFilterStringInput) then FOnFilterStringInput(Self, Key);
+end;
+
+function TSMBLabeledEdit.GetCaption: TCaption;
+begin
+  Result := EditLabel.Caption;
+end;
+
+function TSMBLabeledEdit.GetErrorMsg: String;
+begin
+  if not FAutoValidate then DoValidate;
+  Result := FErrorMsg;
+end;
+
+function TSMBLabeledEdit.GetValid: Boolean;
+begin
+  if not FAutoValidate then DoValidate;
+  Result := FValid;
+end;
+
+function TSMBLabeledEdit.GetValue: Variant;
+begin
+  if not FAutoValidate then DoValidate;
+  case FValueType of
+    vtString:   Result := Text;
+    vtInteger:  Result := FIntValue;
+    vtFloat:    Result := FFloatValue;
+  end;
+end;
+
+procedure TSMBLabeledEdit.KeyPress(var Key: Char);
+begin
+  inherited KeyPress(Key);
+  if FFilterInput then
+    case FValueType of
+      vtString:   FilterStringInput(Key);
+      vtInteger:  FilterIntegerInput(Key);
+      vtFloat:    FilterFloatInput(Key);
+    end;
+end;
+
+procedure TSMBLabeledEdit.SetAutoValidate(const Value: Boolean);
+begin
+  if FAutoValidate <> Value then
+  begin
+    FAutoValidate := Value;
+    if FAutoValidate then DoValidate;
+  end;
+end;
+
+procedure TSMBLabeledEdit.SetCaption(const Value: TCaption);
+begin
+  if EditLabel.Caption <> Value then EditLabel.Caption := Value;
+end;
+
+procedure TSMBLabeledEdit.SetFilterInput(const Value: Boolean);
+begin
+  if FFilterInput <> Value then FFilterInput := Value;
+end;
+
+procedure TSMBLabeledEdit.SetMax(const Value: Extended);
+begin
+  if FMax <> Value then
+  begin
+    FMax := Value;
+    if FAutoValidate then DoValidate;
+  end;
+end;
+
+procedure TSMBLabeledEdit.SetMin(const Value: Extended);
+begin
+    if FMin <> Value then
+  begin
+    FMin := Value;
+    if FAutoValidate then DoValidate;
+  end;
+end;
+
+procedure TSMBLabeledEdit.SetValue(const Value: Variant);
+var
+  StrValue: String;
+begin
+  StrValue := VarToStr(Value);
+  if Text <> StrValue then
+  begin
+    Text := StrValue;
+    Change;
+  end;
+end;
+
+procedure TSMBLabeledEdit.SetValueConstraint(const Value: TValueConstraintType);
+begin
+  if FValueConstraint <> Value then
+  begin
+    FValueConstraint := Value;
+    if FAutoValidate then DoValidate;
+  end;
+end;
+
+procedure TSMBLabeledEdit.SetValueType(const Value: TValueType);
+begin
+  if FValueType <> Value then
+  begin
+    FValueType := Value;
+    if FAutoValidate then DoValidate;
+  end;
+end;
+
+procedure TSMBLabeledEdit.ValidateFloat;
+begin
+  FValid := TryStrToFloatMsg(Caption, Text, FErrorMsg, FFloatValue);
+  if FValid then
+    case FValueConstraint of
+      vctNone:                FValid := True;
+      vctGreaterThan:         FValid := IsGreaterThanFloat(Caption, FFloatValue, FMin, FErrorMsg);
+      vctGreaterThanOrEqual:  FValid := IsGreaterThanOrEqualFloat(Caption, FFloatValue, FMin, FErrorMsg);
+      vctLessThan:            FValid := IsLessThanFloat(Caption, FFloatValue, FMax, FErrorMsg);
+      vctLessThanOrEqual:     FValid := IsLessThanOrEqualFloat(Caption, FFloatValue, FMax, FErrorMsg);
+      vctBetween:             FValid := IsBetweenFloat(Caption, FFloatValue, FMin, FMax, FErrorMsg);
+      vctBetweenOrEqual:      FValid := IsBetweenFloat(Caption, FFloatValue, FMin, FMax, FErrorMsg, True);
+    end;
+end;
+
+procedure TSMBLabeledEdit.ValidateInteger;
+begin
+  FValid := TryStrToIntMsg(Caption, Text, FErrorMsg, FIntValue);
+  case FValueConstraint of
+    vctNone:                FValid := True;
+    vctGreaterThan:         FValid := IsGreaterThanInt(Caption, FIntValue, Trunc(FMin), FErrorMsg);
+    vctGreaterThanOrEqual:  FValid := IsGreaterThanOrEqualInt(Caption, FIntValue, Trunc(FMin), FErrorMsg);
+    vctLessThan:            FValid := IsLessThanInt(Caption, FIntValue, Trunc(FMax), FErrorMsg);
+    vctLessThanOrEqual:     FValid := IsLessThanOrEqualInt(Caption, FIntValue, Trunc(FMax), FErrorMsg);
+    vctBetween:             FValid := IsBetweenInt(Caption, FIntValue, Trunc(FMin), Trunc(FMax), FErrorMsg);
+    vctBetweenOrEqual:      FValid := IsBetweenInt(Caption, FIntValue, Trunc(FMin), Trunc(FMax), FErrorMsg, True);
+  end;
+end;
+
+procedure TSMBLabeledEdit.ValidateString;
+begin
+  case FValueConstraint of
+    vctNone:                FValid := True;
+    vctGreaterThan:         FValid := IsGreaterThanStr(Caption, Text, Trunc(FMin), FErrorMsg);
+    vctGreaterThanOrEqual:  FValid := IsGreaterThanOrEqualStr(Caption, Text, Trunc(FMin), FErrorMsg);
+    vctLessThan:            FValid := IsLessThanStr(Caption, Text, Trunc(FMax), FErrorMsg);
+    vctLessThanOrEqual:     FValid := IsLessThanOrEqualStr(Caption, Text, Trunc(FMax), FErrorMsg);
+    vctBetween:             FValid := IsBetweenStr(Caption, Text, Trunc(FMin), Trunc(FMax), FErrorMsg);
+    vctBetweenOrEqual:      FValid := IsBetweenStr(Caption, Text, Trunc(FMin), Trunc(FMax), FErrorMsg, True);
+  end;
+end;
+
+end.
---
+++ SMBLocalSettings/SMBLocalSettings.pas
@@ -0,0 +1,138 @@
+unit SMBLocalSettings;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Datasnap.DBClient, Data.DB, MidasLib, CRTL;
+
+type
+  { TSMBLocalSettings компонента, предназначенная для сохранения и получения
+    значений. доступ осуществляется как в ассоциативном массиве, где в качестве
+    индекса массива используется строковое значение - ключ (название переменной),
+    а элемент массива содержит строковое значение.
+    Пример использования:
+      var
+        LS: TSMBLocalSettings;
+        FilePath: String;
+      begin
+        LS := TSMBLocalSettings.Create(Self);  // Вариант №1. будет использоваться
+                                    файл settings.xml в корневой папке программы
+        LS := TSMBLocalSettings.Init(Self, 'Путь к файлу/имя файла')
+                                    // Вариант №2. будет использоваться указанный
+                                    путь и имя файла
+        Чтение (Вариант №1, предпочтительный)
+        FilePath := LS['FilePath']; // при этом проверяется наличие файла с настройками
+                                    и наличие сохраненного значения для данного параметра
+                                    если файла нет или или параметр еще не сохранен,
+                                    то возвращается пустая строка, т.е. '',
+                                    иначе возвращается строковое значение этого параметра
+        Чтение (Вариант №2, более длинная запись)
+        FilePath := LS.Value['FilePath'] // Работает как и предыдущий вариант, просто
+                                    запись более длинная
+        Запись (Вариант №1, предпочтительный)
+        LS['FilePath'] := FilePath; // при этом проверяется наличие параметра,
+                                    если параметр есть, то его значение перезаписывается новым,
+                                    если параметра нет, то он создается и ему присваевается значение.
+        Запись (Вариант №2, более длинная запись)
+        LS.Value['FilePath'] := FilePath; // Работает как и предыдущий вариант, просто
+                                    запись более длинная
+      end; }
+
+  TSMBLocalSettings = class(TComponent)
+  private
+    FClientDataSet: TClientDataSet;
+    function GetValue(const Key: string): String;
+    procedure SetValue(const Key: string; const Value: String);
+  public
+    constructor Create(AOwner: TComponent); override;
+    constructor Init(AOwner: TComponent; const AFileName: String = '');
+    destructor Destroy; override;
+    property Value[const Key: string]: String read GetValue write SetValue; default;
+  end;
+
+procedure Register;
+
+implementation
+
+procedure Register;
+begin
+  RegisterComponents('SMB Components', [TSMBLocalSettings]);
+end;
+
+{ TSMBLocalSettings }
+
+{ Этот конструктор используется при визуальном расположении компоненты на форму,
+  либо когда необходимо чтобы файл с локальными настройками хранился в корневой
+  папке программы и назывался settings.xml }
+constructor TSMBLocalSettings.Create(AOwner: TComponent);
+begin
+  Init(AOwner);
+end;
+
+{ Этот конструктор может использоваться только при программном создании объекта класса
+  и имеет возможность указать путь и название файла локальных настроек }
+constructor TSMBLocalSettings.Init(AOwner: TComponent; const AFileName: String = '');
+var
+  FileExist: Boolean;
+begin
+  inherited Create(AOwner);
+  FClientDataSet := TClientDataSet.Create(Self);
+  with FClientDataSet do
+  begin
+    if AFileName = '' then
+      FileName := GetCurrentDir + '\settings.xml'
+    else
+      FileName := AFileName;
+
+    FileExist := FileExists(FileName);
+    if FileExist then
+    begin
+      LoadFromFile;
+      if not (Assigned(FindField('Key')) and Assigned(FindField('Value'))) then
+        FieldDefs.Clear;
+    end;
+
+    if not FileExist or (FieldDefs.Count = 0) then
+    begin
+      FieldDefs.Add('Key',    TFieldType.ftString, 100, True);
+      FieldDefs.Add('Value',  TFieldType.ftString, 100, True);
+      Active := False;
+      CreateDataSet;
+      Active := True;
+    end;
+  end;
+end;
+
+destructor TSMBLocalSettings.Destroy;
+begin
+  FClientDataSet.Free;
+  inherited Destroy;
+end;
+
+function TSMBLocalSettings.GetValue(const Key: string): String;
+begin
+  with FClientDataSet do
+    if Locate('Key', Key, [loPartialKey]) then
+      Result := FieldByName('Value').AsString
+    else
+      Result := ''
+end;
+
+procedure TSMBLocalSettings.SetValue(const Key, Value: String);
+begin
+  with FClientDataSet do
+    if Locate('Key', Key, [loPartialKey]) then
+    begin
+      Edit;
+      FieldByName('Value').AsString := Value;
+      Post;
+    end
+    else
+    begin
+      Append;
+      FieldByName('Key').AsString   := Key;
+      FieldByName('Value').AsString := Value;
+      Post;
+    end;
+end;
+end.
